37ac736133290ff13d0ab544c62293e4a179531c
Lam.Nguyen <lam.nguyen@cognizant.com>
2022-01-02 18:42:02 -0500

    New

--- Inheritance/main.swift
+++ Inheritance/main.swift
@@ -7,5 +7,37 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: - Inheritance
+
+// A class can inherit methods, properties and other characteristics from another class.
+// When one class inherits from another, the inheriting class is known as a subclass, and the class that it inherits from is known as its superclass
+
+// Classes in Swift can call and access methods, properties, and subscripts belonging to their superclass and can provide their own overriding versions.
+
+// Syntax:
+
+// Superclass
+class vehicle {
+    func makeNoise() {
+        print("The vehicle is making some generic noise")
+    }
+}
+
+// Subclass
+class Bicycle : vehicle {
+    
+}
+
+// MARK: - Overriding
+// Overriding is the process where a subclass can provide its own custom implementation of an instance method, type method, instance property, type property, or subscript that it would otherwise inherit from a superclass.
+
+class Airplane : vehicle {
+    override func makeNoise() {
+        print("The airplane makes propellor noise")
+    }
+}
+
+var plane = Airplane()
+plane.makeNoise() // Prints the overridden print statement: The airplane makes propellor noise
+
 
